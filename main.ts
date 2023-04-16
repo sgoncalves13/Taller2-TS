cd49df9d76db4e05cc277f776888eeab8eb769e5
sgoncalves13 <s.goncalves@uniandes.edu.co>
2023-04-16 18:00:41 -0500

    termnado

--- main.ts
+++ main.ts
@@ -3,9 +3,14 @@ import { series } from './data.js';
 
 let componente: HTMLElement = document.getElementById('series')!;
 let componentePromedio: HTMLElement = document.getElementById('promedio')!;
+let foto:HTMLElement = document.getElementById('imagen')!;
+let titulo:HTMLElement = document.getElementById('titulo')!;
+let descripcion: HTMLElement = document.getElementById('descripcion')!;
+let paginaWeb: HTMLElement = document.getElementById('paginaWeb')!;
 
 cargarFilas();
 componentePromedio.innerHTML = `Seasons average: ${calcularPromedio()}`;
+createBotones();
 
 
 function cargarFilas(): void{
@@ -19,7 +24,7 @@ function crearFila(serie: Serie):void{
         ${serie.id}
     </td>
     <td style = "color:#547dde;">
-        ${serie.nombre}
+        <a id = "${serie.id}">${serie.nombre}</a>
     </td>
     <td>
         ${serie.cadena}
@@ -28,6 +33,29 @@ function crearFila(serie: Serie):void{
         ${serie.numtemporadas}
     </td>`);
     componente.appendChild(fila);
+}
+
+function alterCard(id: string){
+    let idR: number = parseInt(id);
+    let serie: Serie = series[idR - 1];
+    foto.setAttribute('src', serie.imagen);
+    titulo.innerHTML = `${serie.nombre}`;
+    descripcion.innerHTML = `${serie.descripcion}`;
+    paginaWeb.setAttribute('href', `${serie.paginaWeb}`);
+    paginaWeb.innerHTML = 'Página Web Serie';
+    const card = document.querySelector('.card');
+    if (card && card.classList.contains('card')) {
+    (card as HTMLElement).style.display = 'block';
+    }
+
+}
+
+
+function createBotones(){
+    series.forEach(c=>{
+        let boton = document.getElementById(`${c.id}`)!;
+        boton.onclick = () => {alterCard(boton.id)};
+    });
 }
 
 function calcularPromedio(): string{
--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,39 @@
 import { series } from './data.js';
 var componente = document.getElementById('series');
 var componentePromedio = document.getElementById('promedio');
+var foto = document.getElementById('imagen');
+var titulo = document.getElementById('titulo');
+var descripcion = document.getElementById('descripcion');
+var paginaWeb = document.getElementById('paginaWeb');
 cargarFilas();
 componentePromedio.innerHTML = "Seasons average: ".concat(calcularPromedio());
+createBotones();
 function cargarFilas() {
     series.forEach(function (c) { return crearFila(c); });
 }
 function crearFila(serie) {
     var fila = document.createElement('tr');
-    fila.innerHTML = ("\n    <td style = \"font-weight: bold;\"> \n        ".concat(serie.id, "\n    </td>\n    <td style = \"color:#547dde;\">\n        ").concat(serie.nombre, "\n    </td>\n    <td>\n        ").concat(serie.cadena, "\n    </td>\n    <td>\n        ").concat(serie.numtemporadas, "\n    </td>"));
+    fila.innerHTML = ("\n    <td style = \"font-weight: bold;\"> \n        ".concat(serie.id, "\n    </td>\n    <td style = \"color:#547dde;\">\n        <a id = \"").concat(serie.id, "\">").concat(serie.nombre, "</a>\n    </td>\n    <td>\n        ").concat(serie.cadena, "\n    </td>\n    <td>\n        ").concat(serie.numtemporadas, "\n    </td>"));
     componente.appendChild(fila);
+}
+function alterCard(id) {
+    var idR = parseInt(id);
+    var serie = series[idR - 1];
+    foto.setAttribute('src', serie.imagen);
+    titulo.innerHTML = "".concat(serie.nombre);
+    descripcion.innerHTML = "".concat(serie.descripcion);
+    paginaWeb.setAttribute('href', "".concat(serie.paginaWeb));
+    paginaWeb.innerHTML = 'Página Web Serie';
+    var card = document.querySelector('.card');
+    if (card && card.classList.contains('card')) {
+        card.style.display = 'block';
+    }
+}
+function createBotones() {
+    series.forEach(function (c) {
+        var boton = document.getElementById("".concat(c.id));
+        boton.onclick = function () { alterCard(boton.id); };
+    });
 }
 function calcularPromedio() {
     var promedio = 0;
